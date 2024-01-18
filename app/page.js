e5fe628b264d485bb75839c90ad0ee3b50cd624a
[BintaSani] <[bintasani992@gmail.com]>
2024-01-18 17:13:55 +0100

    added contact functionality

--- app/page.js
+++ app/page.js
@@ -1,9 +1,25 @@
-
+'use client'
+import { useRef } from "react";
 import Cards from "@/components/cards";
+import emailjs from '@emailjs/browser';
 export default function Home() {
+    const form = useRef();
+    
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_k6xv9eb', 'template_c6w8phw',  form.current, 'mcyz3nP4-VCb6xgX9')
+          .then((result) => {
+              console.log(result.text);
+              e.target.reset();
+              alert('message sent');
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
   return (
     <main className="flex flex-col items-center justify-between py-5 bg-white dark:bg-gray-900">
-      <section class="bg-white dark:bg-gray-900 mb-8">
+      <section id="hero" class="bg-white dark:bg-gray-900 mb-8">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             <div class="mr-auto place-self-center lg:col-span-7">
                 <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Hi, I'm Binta Sani. <br/> A Frontend Web Developer</h1>
@@ -21,7 +37,7 @@ export default function Home() {
             </div>                
         </div>
       </section>
-      <section className="py-8 px-6 min-h-screen mb-[33px] lg:mx-[119px]">
+      <section id="projects" className="py-8 px-6 min-h-screen mb-[33px] lg:mx-[119px]">
         <h2 className="text-4xl font-bold text-white text-center">Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-6 py-8 mx-auto">
           <Cards view="https://crownstores.netlify.app/" first="React" second="Styled-Components" third="Firebase" src="/images/project1.png" head="Crown Store" content="Crown Stores is an e-commerce website developed with React.js which offers simple authentication through Firebase and secure payment processing via Stripe."/>
@@ -30,10 +46,8 @@ export default function Home() {
           <Cards view="https://moviesitetask.netlify.app/" first="React" second="Scss" third="TMDB Api" src="/images/project4.png" head="Movie Box" content="MovieBox is an interactive movie website which offers users the opportunity to explore the top 10 rated movies along with a search functionality to discover specific movies of interest."/>
         </div>
       </section>
-      <section class=" bg-white dark:bg-gray-900 mb-[33px]">
+      <section id="about" class=" bg-white dark:bg-gray-900 mb-[33px]">
         <div class=" items-center py-8 px-4 mx-auto max-w-screen-xl md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-            
-            {/* <img class="w-full hidden dark:block rounded-full h-[600px] " src="/images/aboutimg.jpg" alt="dashboard image"/> */}
             <img class="w-[424px] rounded-full h-[588px] shadow-2xl " src="/images/beautiful_african_developer_anime_style_woman_oper-removebg-preview.png" alt="dashboard image"/>
             <div class="mt-4 md:mt-0">
                 <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">About Me</h2>
@@ -42,7 +56,7 @@ export default function Home() {
             </div>
         </div>
       </section>
-      <section class="bg-white dark:bg-gray-900">
+      <section id="contact" class="bg-white dark:bg-gray-900">
         <div class="container px-6 py-12 mx-auto">
             <div class="lg:flex lg:items-center lg:-mx-6">
                 <div class="lg:w-1/2 lg:mx-6">
@@ -52,7 +66,7 @@ export default function Home() {
 
                     <div class="mt-6 space-y-8 md:mt-8">
                         <p class="flex items-start -mx-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-2 text-blue-500 dark:text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-2 text-gray-400 dark:text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                             </svg>
@@ -63,7 +77,7 @@ export default function Home() {
                         </p>
 
                         <p class="flex items-start -mx-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-2 text-blue-500 dark:text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-2 text-gray-400 dark:text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                             </svg>
 
@@ -71,7 +85,7 @@ export default function Home() {
                         </p>
 
                         <p class="flex items-start -mx-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-2 text-blue-500 dark:text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-2 text-gray-400 dark:text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                             </svg>
 
@@ -84,8 +98,7 @@ export default function Home() {
 
                         <div class="flex mt-4 -mx-1.5 ">
                             
-
-                            <a class="mx-1.5 dark:hover:text-gray-800 text-gray-400 transition-colors duration-300 transform hover:text-gray-700" href="#">
+                            <a class="mx-1.5 dark:hover:text-gray-800 text-gray-400 transition-colors duration-300 transform hover:text-gray-700" href="https://www.linkedin.com/in/binta-sani-123015248/">
                                 <svg class="w-8 h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M15.2 8.80005C16.4731 8.80005 17.694 9.30576 18.5941 10.2059C19.4943 11.1061 20 12.327 20 13.6V19.2H16.8V13.6C16.8 13.1757 16.6315 12.7687 16.3314 12.4687C16.0313 12.1686 15.6244 12 15.2 12C14.7757 12 14.3687 12.1686 14.0687 12.4687C13.7686 12.7687 13.6 13.1757 13.6 13.6V19.2H10.4V13.6C10.4 12.327 10.9057 11.1061 11.8059 10.2059C12.7061 9.30576 13.927 8.80005 15.2 8.80005Z" fill="currentColor" />
                                     <path d="M7.2 9.6001H4V19.2001H7.2V9.6001Z" fill="currentColor" />
@@ -93,15 +106,10 @@ export default function Home() {
                                 </svg>
                             </a>
 
-                            <a class="mx-1.5 dark:hover:text-blue-400 text-gray-400 transition-colors duration-300 transform hover:text-blue-500" href="#">
-                                <svg class="w-8 h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M7 10.2222V13.7778H9.66667V20H13.2222V13.7778H15.8889L16.7778 10.2222H13.2222V8.44444C13.2222 8.2087 13.3159 7.9826 13.4826 7.81591C13.6493 7.64921 13.8754 7.55556 14.1111 7.55556H16.7778V4H14.1111C12.9324 4 11.8019 4.46825 10.9684 5.30175C10.1349 6.13524 9.66667 7.2657 9.66667 8.44444V10.2222H7Z" fill="currentColor" />
-                                </svg>
-                            </a>
-
-                            <a class="mx-1.5 dark:hover:text-blue-400 text-gray-400 transition-colors duration-300 transform hover:text-blue-500" href="#">
-                                <svg class="w-8 h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M11.9294 7.72275C9.65868 7.72275 7.82715 9.55428 7.82715 11.825C7.82715 14.0956 9.65868 15.9271 11.9294 15.9271C14.2 15.9271 16.0316 14.0956 16.0316 11.825C16.0316 9.55428 14.2 7.72275 11.9294 7.72275ZM11.9294 14.4919C10.462 14.4919 9.26239 13.2959 9.26239 11.825C9.26239 10.354 10.4584 9.15799 11.9294 9.15799C13.4003 9.15799 14.5963 10.354 14.5963 11.825C14.5963 13.2959 13.3967 14.4919 11.9294 14.4919ZM17.1562 7.55495C17.1562 8.08692 16.7277 8.51178 16.1994 8.51178C15.6674 8.51178 15.2425 8.08335 15.2425 7.55495C15.2425 7.02656 15.671 6.59813 16.1994 6.59813C16.7277 6.59813 17.1562 7.02656 17.1562 7.55495ZM19.8731 8.52606C19.8124 7.24434 19.5197 6.10901 18.5807 5.17361C17.6453 4.23821 16.51 3.94545 15.2282 3.88118C13.9073 3.80621 9.94787 3.80621 8.62689 3.88118C7.34874 3.94188 6.21341 4.23464 5.27444 5.17004C4.33547 6.10544 4.04628 7.24077 3.98201 8.52249C3.90704 9.84347 3.90704 13.8029 3.98201 15.1238C4.04271 16.4056 4.33547 17.5409 5.27444 18.4763C6.21341 19.4117 7.34517 19.7045 8.62689 19.7687C9.94787 19.8437 13.9073 19.8437 15.2282 19.7687C16.51 19.708 17.6453 19.4153 18.5807 18.4763C19.5161 17.5409 19.8089 16.4056 19.8731 15.1238C19.9481 13.8029 19.9481 9.84704 19.8731 8.52606ZM18.1665 16.5412C17.8881 17.241 17.349 17.7801 16.6456 18.0621C15.5924 18.4799 13.0932 18.3835 11.9294 18.3835C10.7655 18.3835 8.26272 18.4763 7.21307 18.0621C6.51331 17.7837 5.9742 17.2446 5.69215 16.5412C5.27444 15.488 5.37083 12.9888 5.37083 11.825C5.37083 10.6611 5.27801 8.15832 5.69215 7.10867C5.97063 6.40891 6.50974 5.8698 7.21307 5.58775C8.26629 5.17004 10.7655 5.26643 11.9294 5.26643C13.0932 5.26643 15.596 5.17361 16.6456 5.58775C17.3454 5.86623 17.8845 6.40534 18.1665 7.10867C18.5843 8.16189 18.4879 10.6611 18.4879 11.825C18.4879 12.9888 18.5843 15.4916 18.1665 16.5412Z" fill="currentColor" />
+                            <a class="mx-1.5 flex items-center dark:hover:text-gray-800 text-gray-400 transition-colors duration-300 transform hover:text-gray-700" href="https://github.com/BintaSani">
+                                
+                                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
+                                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" fill='currentColor'/>
                                 </svg>
                             </a>
                         </div>
@@ -113,23 +121,23 @@ export default function Home() {
                         class="w-full px-8 py-10 mx-auto overflow-hidden bg-white rounded-lg shadow-2xl dark:bg-gray-900 lg:max-w-xl shadow-gray-300/50 dark:shadow-black/50">
                         <h1 class="text-lg font-medium text-gray-700">Contact Form</h1>
 
-                        <form class="mt-6">
+                        <form class="mt-6" ref={form} onSubmit={sendEmail} >
                             <div class="flex-1">
                                 <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Full Name</label>
-                                <input type="text" placeholder="John Doe" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                                <input type="text" name='fullName' placeholder="John Doe" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700  focus:ring-gray-100 dark:focus:ring-gray-800 focus:outline-none focus:ring " required/>
                             </div>
 
                             <div class="flex-1 mt-6">
                                 <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>
-                                <input type="email" placeholder="johndoe@example.com" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                                <input type="email" name='email' placeholder="johndoe@example.com" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:ring-gray-100 dark:focus:ring-gray-800 focus:outline-none focus:ring " required/>
                             </div>
 
                             <div class="w-full mt-6">
                                 <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>
-                                <textarea class="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Message"></textarea>
+                                <textarea name='message' class="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:ring-gray-100 dark:focus:ring-gray-800 focus:outline-none focus:ring " placeholder="Message" required></textarea>
                             </div>
 
-                            <button class="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-700 hover:bg-gray-800 rounded-md focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                            <button type="submit" class="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-700 hover:bg-gray-800 rounded-md focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
                                 get in touch
                             </button>
                         </form>
--- components/header.jsx
+++ components/header.jsx
@@ -4,18 +4,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 function header() {
-    // const [theme, setTheme] = useState("dark");
-
-    // const toggleTheme=()=>{
-    //     if(theme === "dark"){
-    //         document.documentElement.classList.toggle('dark', !theme);
-    //         setTheme("light")
-
-    //     }else{
-    //         document.documentElement.classList.toggle('dark', !theme);
-    //         setTheme("dark")
-    //     }
-    // }
     const [darkMode, setDarkMode] = useState(true);
 
   const toggleTheme = () => {
@@ -27,13 +15,13 @@ function header() {
         <header>
             <nav class="bg-white px-4 lg:px-6 pb-2.5 dark:bg-gray-900 pt-7">
                 <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <a href="https://flowbite.com" class="flex items-center">
+                    <a href="/" class="flex items-center">
                         <img src="/logo-white.svg" class="mr-3 hidden dark:flex h-6 sm:h-9" alt="Binta Logo" />
                         <img src="/logo-dark.svg" class="mr-3 dark:hidden h-6 sm:h-9" alt="Binta Logo" />
                         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Binta</span>
                     </a>
                     <div class="flex items-center lg:order-2">
-                        <button onClick={toggleTheme} class="w-14 text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-800 focus:outline-none dark:focus:ring-primary-800">
+                        <button onClick={toggleTheme} class="w-13 lg:w-14 text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-800 focus:outline-none dark:focus:ring-primary-800">
                             <div class="hidden dark:flex">
                             <FontAwesomeIcon
                                 id='sun'
@@ -60,13 +48,13 @@ function header() {
                     <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                         <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
-                                <a href="#" class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-gray-400 dark:hover:text-white" >Projects</a>
+                                <a href="#projects" class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-gray-400 dark:hover:text-white" >Projects</a>
                             </li>
                             <li>
-                                <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">About Me</a>
+                                <a href="#about" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">About Me</a>
                             </li>
                             <li>
-                                <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                                <a href="#contact" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
                             </li>
                         </ul>
                     </div>
