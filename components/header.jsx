a917ce28454bdad037e7efa1996fa590acfc6376
[BintaSani] <[bintasani992@gmail.com]>
2024-01-19 22:58:27 +0100

    mobile menu

--- components/header.jsx
+++ components/header.jsx
@@ -5,6 +5,11 @@ import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 function header() {
     const [darkMode, setDarkMode] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const show = () => {
+        setIsOpen(!isOpen);
+    }
 
   const toggleTheme = () => {
     setDarkMode(!darkMode);
@@ -39,11 +44,16 @@ function header() {
                             />
                             </div>
                         </button>
-                        <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                        <button onClick={show} data-collapse-toggle="mobile-menu-2" type="button" class="relative inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                             <span class="sr-only">Open main menu</span>
                             <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                             <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         </button>
+                        {isOpen && (<div className='absolute z-50 right-4 p-3 top-20 shadow-md w-40 bg-primary-700 rounded'>
+                            <div class="block py-2 flex justify-center items-center pr-4 pl-3 text-white rounded bg-primary-700 dark:text-gray-400 hover:text-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 dark:hover:text-white"><a href="#projects">Projects</a></div>
+                            <div class="block py-2 flex justify-center items-center pr-4 pl-3 text-white rounded bg-primary-700 dark:text-gray-400 hover:text-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 dark:hover:text-white"><a href="#about">About Me</a></div>
+                            <div class="block py-2 flex justify-center items-center pr-4 pl-3 text-white rounded bg-primary-700 dark:text-gray-400 hover:text-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 dark:hover:text-white"><a href="#contact">Contact</a></div>
+                        </div>)}
                     </div>
                     <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                         <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
