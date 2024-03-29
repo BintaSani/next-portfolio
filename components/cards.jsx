eb204ffb31ad236358fac6cd41ad0c8be82284b5
BintaSani <bintasani992@gmail.com>
2024-03-29 16:19:49 +0100

    justified project description text

--- components/cards.jsx
+++ components/cards.jsx
@@ -20,7 +20,7 @@ function cards({src, head, content, first, second, third,  view}) {
             <img class="w-full" src={src} alt="Sunset in the mountains"/>
             <div class="px-6 py-4">
                 <div class="font-bold text-white text-xl mb-2">{head}</div>
-                <p class="text-gray-500 text-base">{content}</p>
+                <p class="text-gray-500 text-base text-justify">{content}</p>
             </div>
             <div class="px-6 pt-4 pb-2">
                 <span class="inline-block bg-gray-700 rounded-full px-3 py-1 text-sm font-semibold text-gray-200 mr-2 mb-2">{first}</span>
