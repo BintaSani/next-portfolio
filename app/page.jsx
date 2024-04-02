f3b56fc39952d1e4f821792a65e8c19a265785ed
BintaSani <bintasani992@gmail.com>
2024-04-02 11:12:45 +0100

    updated cv

--- app/page.jsx
+++ app/page.jsx
@@ -53,7 +53,7 @@ export default function Home() {
                     Get in touch
                     <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </motion.a>
-                <motion.a whileTap={{ scale: 0.8 }} href="https://drive.google.com/uc?export=download&id=1fSXjclaZbc8HFp8W-5rU2bk2ae4DGQuA" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                <motion.a whileTap={{ scale: 0.8 }} href="https://drive.google.com/uc?export=download&id=1BV9455hIuXgQHAPI37nj0QrMpmMyY4b0" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                     Download Cv
                 </motion.a> 
             </motion.div>
